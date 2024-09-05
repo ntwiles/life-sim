73d93be930e0407db8751bb25a55802cfe1af9ab
Nathan Wiles <ntwiles@gmail.com>
2024-09-05 00:09:53 -0400

    Cleanup comments.

--- src/genome/mutation.rs
+++ src/genome/mutation.rs
@@ -8,7 +8,7 @@ pub fn mutate_genome(genome: &mut Vec<Gene>) {
     let element_index = rng.gen_range(0..genome.len());
     let bit_index16 = 1 << rng.gen_range(0..16);
 
-    let chance: f32 = rng.gen(); // Generates a random float between 0.0 and 1.0
+    let chance: f32 = rng.gen();
     let gene = &mut genome[element_index];
 
     if chance < 0.05 {
@@ -20,7 +20,6 @@ pub fn mutate_genome(genome: &mut Vec<Gene>) {
     } else if chance < 0.2 {
         gene.target_instance ^= bit_index16;
     } else {
-        // TODO: One of these things is not like the other.
         let weight_mutation = rng.gen_range(-0.1..0.1);
         gene.weight += weight_mutation;
     }
