aafa20dd472cb6574818c1b636fe4033f9c8ae3f
Nathan Wiles <ntwiles@gmail.com>
2024-09-04 22:17:25 -0400

    Apply better mutations.

--- src/genome/mutation.rs
+++ src/genome/mutation.rs
@@ -9,12 +9,19 @@ pub fn flip_random_bit(genome: &mut Vec<Gene>) {
     let bit_index16 = 1 << rng.gen_range(0..16);
 
     let chance: f32 = rng.gen(); // Generates a random float between 0.0 and 1.0
+    let gene = &mut genome[element_index];
 
-    if chance < 0.33 {
-        genome[element_index].source_discriminant ^= bit_index16;
-    } else if chance < 0.66 {
-        genome[element_index].target_discriminant ^= bit_index16;
-    } else {
-        // TODO: mutate weight.
+    if chance < 0.2 {
+        gene.source_discriminant ^= bit_index16;
+    } else if chance < 0.4 {
+        gene.source_instance ^= bit_index16;
+    } else if chance < 0.6 {
+        gene.target_discriminant ^= bit_index16;
+    } else if chance < 0.8 {
+        gene.target_instance ^= bit_index16;
+    } else if chance < 1.0 {
+        // TODO: One of these things is not like the other.
+        let weight_mutation = rng.gen_range(-0.1..0.1);
+        gene.weight += weight_mutation;
     }
 }
